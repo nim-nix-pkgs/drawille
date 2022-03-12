77eb2a7abaa057b6a929b3abdb2c79c8999138cc
hugosenari <hugosenari@gmail.com>
2022-03-12 02:46:45 -0300

    chore: re index drawille

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Drawing in terminal with Unicode Braille characters.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-drawille-master.flake = false;
+  inputs.src-drawille-master.owner = "PMunch";
+  inputs.src-drawille-master.ref   = "refs/heads/master";
+  inputs.src-drawille-master.repo  = "drawille-nim";
+  inputs.src-drawille-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-drawille-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-drawille-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
